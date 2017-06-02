d6578b649c8e18b7acd5992e42828de25a97c48f
Kuba <quba560@gmail.com>
2017-06-03 00:42:54 +0200

    prototype / to be fixed

--- app/components/Panel.jsx
+++ app/components/Panel.jsx
@@ -1,22 +1,38 @@
 var React = require('react');
 
+var minTemp = 17;
+
 var Panel = React.createClass({
   componentDidMount: function() {
     var elem = new Foundation.Slider($('.slider'));
   },
+  getInitialState: function () {
+    return{
+      setTemp: this.refs.location.value;
+    };
+  },
+  onValueChange: function () {
+    this.setState({
+      setTemp: minTemp + this.refs.location.value*0.1
+    });
+  },
   onFormSubmit: function(e){
     e.preventDefault();
     var location = this.refs.location.value;
-    alert(location);
-    this.forceUpdate();
+    var value = minTemp + location*0.1;
+    alert(value);
+    this.forceUpdate(); // <- to be removed
   },
   render: function() {
-    var curTemp = 20;
-    var temp=this.location;
+    var {setTemp} = this.state;
+    var curTemp = 24; // <-- API
+    curTemp = (curTemp-minTemp)*10;
+    var temp=setTemp; // not necessary, can use setTemp later
+    var random = Math.random(); // test, to be removed
 
-    function showSelectedTemp(shit){
+    function showSelectedTemp(){
       return(
-        <div>
+        <div style={{border:'2px solid red;height:50px'}}>
           {temp}
         </div>
       )
@@ -26,15 +42,19 @@ var Panel = React.createClass({
         <div className="row">
           <div className="borderName">now</div>
           <div className="currentStats">
-            Temp: 28
+            Temp: {random}
           </div>
           <div className="borderName">control panel</div>
           <div className="controlPanel">
-            <div className="slider vertical" data-slider data-initial-start={curTemp} data-end="200" data-vertical="true">
-              <span className="slider-handle" data-slider-handle role="slider" tabIndex="1" aria-controls="sliderOutput1"></span>
+            <div className="slider vertical" data-slider data-initial-start={curTemp} data-step="5" data-end="110" data-vertical="true">
+              <span className="slider-handle" data-slider-handle role="slider" tabIndex="1" aria-controls="sliderOutput1"
+             onClick={this.onValueChange}
+             onMouseUp={this.onValueChange}
+             onMouseMove={this.onValueChange}
+             onMouseDown={this.onValueChange}></span>
               <span className="slider-fill" data-slider-fill></span>
-              <input type="hidden" ref="location"/>
             </div>
+            <input type="number" id="sliderOutput1" ref="location" onProgress={this.onValueChange}/>
           </div>
           <div className="borderButton">
               <form onSubmit={this.onFormSubmit}>
@@ -42,7 +62,7 @@ var Panel = React.createClass({
               </form>
           </div>
         </div>
-        {showSelectedTemp("okay")}
+        {showSelectedTemp()}
       </div>
     )
   }
